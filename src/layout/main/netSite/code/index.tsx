ad279c3a6742e620533dd7d2714f9ccda544712a
chen <596487930@qq.com>
2022-05-04 14:01:29 +0800

    完成连接数据库，请求

--- src/layout/main/netSite/code/index.tsx
+++ src/layout/main/netSite/code/index.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import { experimentalStyled as styled } from '@mui/material/styles';
 import MainHeader from '../../components/mainHeader'
 
@@ -25,6 +27,7 @@ import loginBgImg from '@/static/loginBg.jpg'
 
 import { useSelector, useDispatch } from "react-redux";
 import { increment, IINCREMENTAction } from '@/redux/count/actions'
+import { getNetSiteCodeLinkList } from '@/request/api/code'
 
 interface ExpandMoreProps extends IconButtonProps {
   expand: boolean;
@@ -53,27 +56,50 @@ const Item = styled(Paper)(({ theme }) => ({
   boxShadow: 'none'
 }));
 
+type codeType = {
+  detail: String,
+  link: String,
+  linkSum: Number,
+  netId: Number,
+  title: String,
+  type: 'code' | 'design' | 'amuse'
+}
+
 export default function Code() {
   const countState = useSelector((state) => state);
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   const [expanded, setExpanded] = React.useState(false);
+  const [linkList, setLinkList] = React.useState([]);
 
+  React.useEffect(() => {
+    const init = async () => {
+      const res = await getNetSiteCodeLinkList()
+      const getCodes = res.data.filter((item: codeType) => item.type === 'code')
+      setLinkList(getCodes) //filter out the type of code module
+    }
+    init()
+  }, [])
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
 
+  //click on the jump code modules
+  const jumpCodeSite = (url: any) => {
+    window.location.href = url
+  }
   const incrementClick = () => {
     dispatch(increment())
   }
 
   return (
-    <Box >
-      <div onClick={incrementClick}>
+    <Box>
+      <div>
         <MainHeader ></MainHeader>
       </div>
       <Grid container spacing={{ xs: 0, md: 0 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-        {Array.from(Array(10)).map((_, index) => (
+        {linkList.map((link: codeType, index) => (
           <Grid item xs={2} sm={4} md={3} key={index}>
             <Item>
               <Card sx={{ maxWidth: 345 }}>
@@ -88,7 +114,7 @@ export default function Code() {
                       <MoreVertIcon />
                     </IconButton>
                   }
-                  title="Shrimp and Chorizo "
+                  title={link.title}
                   subheader="September 14, 2016"
                 />
                 <CardMedia
@@ -99,7 +125,7 @@ export default function Code() {
                 />
                 <CardContent>
                   <Typography variant="body2" color="text.secondary">
-                    This impressive paella is a perfect party dish and a fun meal to cook
+                    {link.detail}
                   </Typography>
                 </CardContent>
                 <CardActions disableSpacing>
@@ -107,7 +133,7 @@ export default function Code() {
                     <FavoriteIcon />
                   </IconButton>
                   <IconButton aria-label="share">
-                    <ShareIcon />
+                    <ShareIcon onClick={() => { jumpCodeSite(link.link) }} />
                   </IconButton>
                   <ExpandMore
                     expand={expanded}
@@ -120,31 +146,7 @@ export default function Code() {
                 </CardActions>
                 <Collapse in={expanded} timeout="auto" unmountOnExit>
                   <CardContent>
-                    <Typography paragraph>Method:</Typography>
-                    <Typography paragraph>
-                      Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-                      aside for 10 minutes.
-                    </Typography>
-                    <Typography paragraph>
-                      Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-                      medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-                      occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-                      large plate and set aside, leaving chicken and chorizo in the pan. Add
-                      pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-                      stirring often until thickened and fragrant, about 10 minutes. Add
-                      saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-                    </Typography>
-                    <Typography paragraph>
-                      Add rice and stir very gently to distribute. Top with artichokes and
-                      peppers, and cook without stirring, until most of the liquid is absorbed,
-                      15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-                      mussels, tucking them down into the rice, and cook again without
-                      stirring, until mussels have opened and rice is just tender, 5 to 7
-                      minutes more. (Discard any mussels that don&apos;t open.)
-                    </Typography>
-                    <Typography>
-                      Set aside off of the heat to let rest for 10 minutes, and then serve.
-                    </Typography>
+                    <Typography paragraph> {link.detail}</Typography>
                   </CardContent>
                 </Collapse>
               </Card>
